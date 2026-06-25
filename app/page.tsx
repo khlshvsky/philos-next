dbc22a36670ecc2e543d8c986ca9d0409b41666b
Bamboucheur <45428125+khlshvsky@users.noreply.github.com>
2026-06-25 22:21:50 +0200

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 import { useState, useEffect, useMemo } from 'react'
 import Link from 'next/link'
 import { BOOKS, ERA_ORDER, ERA_META } from '@/lib/books'
-import EraTag from '@/components/EraTag'
 
 const DONE_KEY = 'philos-done-v2'
 
