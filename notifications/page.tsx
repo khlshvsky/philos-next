0dbdecb181764f7bed93ee4fb42d7901dcb29d4b
Bamboucheur <45428125+khlshvsky@users.noreply.github.com>
2026-06-26 20:18:50 +0200

    notifications/page.tsx

--- notifications/page.tsx
+++
@@ -1,9 +0,0 @@
-import NotificationsClient from './NotificationsClient'
-
-export default function NotificationsPage() {
-  return <NotificationsClient />
-}
-
-export const metadata = {
-  title: 'Уведомления | Философский канон',
-}
