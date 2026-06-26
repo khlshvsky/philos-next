b06a941296964d9535229a5d8135d9c14829a51e
Bamboucheur <45428125+khlshvsky@users.noreply.github.com>
2026-06-26 09:56:42 +0200

    next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,13 @@
 import type { NextConfig } from 'next'
 
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: isProd ? '/philos-next' : '',
+  assetPrefix: isProd ? '/philos-next/' : '',
   images: {
     unoptimized: true,
-    remotePatterns: [
-      { protocol: 'https', hostname: 'upload.wikimedia.org' },
-      { protocol: 'https', hostname: '*.wikipedia.org' },
-    ],
   },
 }
 
