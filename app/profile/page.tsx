4ce9776062e236dccb87a7a008f0c8de28919583
Bamboucheur <45428125+khlshvsky@users.noreply.github.com>
2026-06-26 14:07:36 +0200

    page.tsx

---
+++ app/profile/page.tsx
@@ -0,0 +1,9 @@
+import ProfileClient from './ProfileClient'
+
+export default function ProfilePage() {
+  return <ProfileClient />
+}
+
+export const metadata = {
+  title: 'Мой профиль | Философский канон',
+}
